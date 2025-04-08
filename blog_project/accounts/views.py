836d0b931198d57e895b9e98c91c52217e77a64c
Behzad Amini <behzadamini.dev@gmail.com>
2025-04-08 23:44:17 +0330

    Created Login and Signup

---
+++ blog_project/accounts/views.py
@@ -0,0 +1,29 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+
+def login_view(request):
+
+    if request.method == "POST":
+        username = request.POST["username"]
+        password = request.POST["password"]
+        user = authenticate(request, username= username, password=password)
+        
+        if user is not None:
+            login(request, user=user)
+            return redirect("/blog")
+        else:
+            return redirect("/accounts/login")
+
+    return render(request, 'accounts/signin.html')
+
+
+def logout_view(request):
+    if request.user.is_authenticated:
+        logout(request)
+        return redirect('/')
+    else:
+        return redirect('/accounts/login')
+
+
+def signup_view(request):
+    return render(request, 'accounts/signin.html')
