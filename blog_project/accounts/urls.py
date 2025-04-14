b10ec98c3bd83931cff909d73b372cc6175f9b0d
Behzad Amini <behzadamini.dev@gmail.com>
2025-04-14 23:56:25 +0330

    forgot passowrd

--- blog_project/accounts/urls.py
+++ blog_project/accounts/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from django.contrib.auth import views as auth_views
 from .import views
 
 app_name = "accounts"
@@ -8,4 +9,11 @@ urlpatterns = [
     path('logout', views.logout_view, name='logout'),
     path('signup', views.signup_view, name='signup'),
     path('forgot', views.forgot_view, name='forgot'),
+
+    path('reset_password', auth_views.PasswordResetView.as_view(), name='reset_password'),
+    path('reset_password_sent', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
+    path('reset/<uidb64>/<token>', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+    path('reset_password_complete', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
 ]
+
+
