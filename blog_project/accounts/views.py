519556cd18812ba54750bcb6492540c646922196
Behzad Amini <beh.amini@abfa.local>
2025-05-12 11:58:03 +0330

    signup form

--- blog_project/accounts/views.py
+++ blog_project/accounts/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout, models
+from django.contrib import messages
 
 def login_view(request):
 
@@ -26,16 +27,18 @@ def logout_view(request):
 
 
 def signup_view(request):
-
-    if request.method == "POST":
-        user = models.User.objects.create_user(
-            username=request.POST["email"],
-            password=request.POST["password"],
-            email=request.POST["email"],
-            first_name=request.POST["first_name"],
-            last_name=request.POST["last_name"],
-        )
-        user.save()
+    try:
+        if request.method == "POST":
+            user = models.User.objects.create_user(
+                username=request.POST["email"],
+                password=request.POST["password"],
+                email=request.POST["email"],
+                first_name=request.POST["first_name"],
+                last_name=request.POST["last_name"],
+            )
+            user.save()
+    except Exception as e:
+        messages.error(request, f"خطا در ثبت نام: {str(e)}")
     return render(request, 'accounts/signup.html')
 
 def forgot_view(request):
