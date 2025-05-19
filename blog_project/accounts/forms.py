51a8f8fc8eff6aca7ff1afb1e54ab1833a799c8f
Behzad Amini <behzadamini.dev@gmail.com>
2025-05-19 19:22:29 +0330

    reset pasword fix

--- blog_project/accounts/forms.py
+++ blog_project/accounts/forms.py
@@ -7,13 +7,13 @@ class CustomSetPasswordForm(SetPasswordForm):
     def clean_new_password1(self):
         password = self.cleaned_data.get('new_password1')
         if not re.search(r'[A-Z]', password):
-            raise ValidationError('رمز عبور باید حداقل یک حرف بزرگ داشته باشد.')
+            raise ValidationError('Password must contain at least one uppercase letter.')
         if not re.search(r'[a-z]', password):
-            raise ValidationError('رمز عبور باید حداقل یک حرف کوچک داشته باشد.')
+            raise ValidationError('Password must contain at least one lowercase letter.')
         if not re.search(r'[0-9]', password):
-            raise ValidationError('رمز عبور باید حداقل یک عدد داشته باشد.')
+            raise ValidationError('Password must contain at least one number.')
         if not re.search(r'[\W_]', password):
-            raise ValidationError('رمز عبور باید حداقل یک نماد داشته باشد.')
+            raise ValidationError('Password must contain at least one special character.')
         if len(password) < 8:
-            raise ValidationError('رمز عبور باید حداقل ۸ کاراکتر باشد.')
+            raise ValidationError('Password must be at least 8 characters long.')
         return password
